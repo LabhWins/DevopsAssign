8ef8deb7872e0e6636f15deded14a9ab12e86a19
LabhWins <kadulabhesh@gmail.com>
2025-02-11 14:37:31 +0530

    Update NewFile.java

--- NewFile.java
+++ NewFile.java
@@ -1,7 +1,1 @@
-Hey Labhesh Here
-public class BeautifulWorld {
-    public static void main(String[] args) {
-        // Print a beautiful message to the console
-        System.out.println("Hello, Beautiful World!");
-    }
-}
+console.log("heyyyy")
